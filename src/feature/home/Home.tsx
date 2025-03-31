e7dcf72c50dbb97b3bc5c14e83d8137dbc826d25
jungu0416 <jungu0416@naver.com>
2025-03-31 14:25:33 +0900

    Style: font 추가

--- src/feature/home/Home.tsx
+++ src/feature/home/Home.tsx
@@ -27,12 +27,20 @@ const UserList: React.FC = () => {
 
   return (
     <div>
-      <h1 className="mt-10">margin top tailwind</h1>
+      <h1 className="mt-10 p-8">margin top tailwind</h1>
       <ul>
         {users?.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+        <h1 className="font-black text-4xl">🚀 Tailwind + Pretendard Variable</h1>
+        <p className="font-light text-lg mt-4">이 텍스트는 Light (300)입니다.</p>
+        <p className="font-normal text-lg">이 텍스트는 Regular (400)입니다.</p>
+        <p className="font-medium text-lg">이 텍스트는 Medium (500)입니다.</p>
+        <p className="font-bold text-lg">이 텍스트는 Bold (700)입니다.</p>
+        <p className="font-black text-lg">이 텍스트는 Black (900)입니다.</p>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'], // 기본 sans-serif를 Pretendard로 변경
+      },
+    },
   },
   plugins: [],
 }
