2e2a1f8b535a57e6de76c91b590d9bcafba695d9
jungu0416 <jungu0416@naver.com>
2025-04-02 17:00:09 +0900

    Style: global css, Dark mode 추가

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '../layout/Layout'
 import Home from '@/feature/home/Home'
+import Design from '@/feature/design/Design'
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/design-guide" element={<Design />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/feature/design/Design.tsx
@@ -0,0 +1,19 @@
+const Design = () => {
+  return (
+    <>
+      <p className="bg-basic-white text-basic-black">bg-basic-white text-basic-black 입니다</p>
+      <p className="bg-basic-black text-basic-white">bg-basic-black text-basic-white 입니다</p>
+      <p className="text-gray-light">text-gray-light 입니다</p>
+      <p className="text-gray-light-medium">text-gray-light-medium 입니다</p>
+      <p className="text-gray-medium">text-gray-medium 입니다</p>
+      <p className="text-gray-medium-dark">text-gray-medium-dark 입니다</p>
+      <p className="text-gray-dark">text-gray-dark 입니다</p>
+      <p className="text-gray-dark-reply">text-gray-dark-reply 입니다</p>
+      <p className="text-main-primary">text-main-primary 입니다</p>
+      <p className="text-main-warning">text-main-warning 입니다</p>
+      <p className="text-main-success">text-main-success 입니다</p>
+    </>
+  )
+}
+
+export default Design
