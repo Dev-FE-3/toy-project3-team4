9759f8543c8daa1501111862cdc766e39cbebba4
jungu0416 <jungu0416@naver.com>
2025-04-02 22:40:36 +0900

    Feat: shadcn 추가, Layout 설정

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/shared/lib/shadcn/ui/button'
 import { Outlet } from 'react-router'
 
 function Layout() {
@@ -12,6 +13,11 @@ function Layout() {
             스트리밍 <span className="text-main-primary">1등</span> 플랫폼
           </p>
           <p className="text-main-primary">플레이요!</p>
+          <div className="mt-4 flex flex-wrap gap-1">
+            <Button className="bg-main-warning hover:bg-red-600 text-white"># 신규 콘텐츠 업데이트</Button>
+            <Button className="bg-main-primary hover:bg-blue-600 text-basic-white"># 인기 급상승</Button>
+            <Button className="bg-main-success hover:bg-purple-600 text-white"># 오직 플레이요에서</Button>
+          </div>
         </section>
         <footer className="flex mt-auto">
           <img src="/public/image/download/google-play-rd.png" className="w-2/6" />
