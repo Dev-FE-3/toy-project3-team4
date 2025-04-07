bfaaa49b5cb687052fd052e001bbc66b1ec9f525
jungu0416 <jungu0416@naver.com>
2025-04-07 22:54:53 +0900

    Feat: 시청기록 youtube API

--- src/feature/app-settings/api/useViewHistory.ts
+++ src/feature/app-settings/api/useViewHistory.ts
@@ -1,13 +1,12 @@
 import axiosInstance from '@/shared/lib/axios/axiosInstance'
 import { useQuery } from '@tanstack/react-query'
 
-const fetchViewHistory = async () => {
-  const response = await axiosInstance.get(`/view?select=*&order=created_at.desc`)
-
-  return response.data
-}
-
 const useViewHistory = () => {
+  const fetchViewHistory = async () => {
+    const response = await axiosInstance.get(`/view?select=*&order=created_at.desc`)
+
+    return response.data
+  }
   return useQuery({
     queryKey: ['view-history'],
     queryFn: fetchViewHistory,
--- src/feature/app-settings/components/ViewHistory.tsx
+++ src/feature/app-settings/components/ViewHistory.tsx
@@ -2,9 +2,11 @@ import { Button } from '@/shared/lib/shadcn/ui/button'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import useViewHistory from '../api/useViewHistory'
+import useYoutubeLinkInfo from '../api/useYoutubeLinkInfo'
 
 const ViewHistory = () => {
   const { data: view, isLoading, error } = useViewHistory()
+  const youtubeLinks = useYoutubeLinkInfo(view)
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error occurred</div>
@@ -20,17 +22,30 @@ const ViewHistory = () => {
 
       <ul className="mb-[15px] mt-[15px]">
         <Swiper spaceBetween={15} slidesPerView={'auto'}>
-          {view.map((item, idx) => (
-            <SwiperSlide key={idx} style={{ width: '140px' }}>
-              <li className="flex h-[130px] w-[140px] flex-col">
-                <img className="h-[86px] rounded-md border-none" src="https://i.ytimg.com/vi/Ht17A6Sb_og/mqdefault.jpg"></img>
-                <div className="mt-[5px]">
-                  <p className="overflow-hidden text-ellipsis whitespace-nowrap">아이유의 전성기 노래를 들어</p>
-                  <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[12px] text-gray-medium-dark">아이유팬1호</p>
-                </div>
-              </li>
-            </SwiperSlide>
-          ))}
+          {youtubeLinks.map((item, idx) => {
+            if (item.isLoading || item.isError || !item.data) return null
+            const data = item.data.items[0]
+            const { id, kind, snippet, contentDetails } = data
+            const { title, channelTitle } = snippet
+            const itemCount = contentDetails?.itemCount
+
+            const isPlaylist = kind === 'youtube#playlist'
+            const thumbnailUrl = isPlaylist
+              ? snippet?.thumbnails?.high?.url // YouTube API가 제공하는 플레이리스트 썸네일
+              : `https://img.youtube.com/vi/${id}/maxresdefault.jpg`
+
+            return (
+              <SwiperSlide key={idx} style={{ width: '140px' }}>
+                <li className="flex flex-col">
+                  <img className="h-[86px] max-h-[86px] min-h-[86px] w-full rounded-md border-none object-cover" src={thumbnailUrl} />
+                  <div className="mt-[5px]">
+                    <h4 className="overflow-hidden text-ellipsis whitespace-nowrap">{title}</h4>
+                    <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[12px] text-gray-medium-dark">{channelTitle}</p>
+                  </div>
+                </li>
+              </SwiperSlide>
+            )
+          })}
         </Swiper>
       </ul>
     </article>
