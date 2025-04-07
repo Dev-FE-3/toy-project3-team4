ac42f6be5b0b15ca2e25f8709bb78592b8cc64e6
mirinae700 <mirinae219@gmail.com>
2025-04-08 00:22:47 +0900

    Design: 홈 스타일링

--- src/feature/home/index.tsx
+++ src/feature/home/index.tsx
@@ -1,33 +1,70 @@
-import React from 'react'
-import { useYoutubeVideoInfo } from '@/shared/util/youtube'
-import { useUserList } from './api/useUserList'
+import React, { useEffect } from 'react'
+import { YouTubeVideoItem } from './type/IVideoTypes'
+import { YouTubePlaylistItem } from './type/IPlaylistTypes'
+import VideoItem from './components/VideoItem'
+import PlayListItem from './components/PlayListItem'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { fetchYoutubePopularPlaylist, fetchYoutubePopularVideo } from '@/shared/util/youtube'
 
 const Home: React.FC = () => {
-  const { data: users, isLoading: isUsersLoading, error: userError } = useUserList()
-  const { data: video, isLoading: isVideoLoading, error: videoError } = useYoutubeVideoInfo('60YQrsnduUc')
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, error } = useInfiniteQuery({
+    queryKey: ['fetchYoutubePopularVideo'],
+    queryFn: fetchYoutubePopularVideo,
+    getNextPageParam: (lastPage) => lastPage.nextPageToken || undefined,
+    initialPageParam: '',
+    staleTime: 1000 * 60 * 5,
+  })
 
-  if (isUsersLoading || isVideoLoading) return <p>Loading...</p>
-  if (userError instanceof Error) return <p>{userError.message}</p>
-  if (videoError instanceof Error) return <p>{videoError.message}</p>
+  const {
+    data: playlistData,
+    fetchNextPage: fetchNextPlaylistPage,
+    hasNextPage: hasNextPlaylistPage,
+    isFetchingNextPage: isFetchingNextPlaylistPage,
+    isLoading: isLoadingPlaylist,
+    error: errorPlaylist,
+  } = useInfiniteQuery({
+    queryKey: ['fetchYoutubePopularPlaylist'],
+    queryFn: fetchYoutubePopularPlaylist,
+    getNextPageParam: (lastPage) => lastPage.nextPageToken || undefined,
+    initialPageParam: '',
+    staleTime: 1000 * 60 * 5,
+  })
+  console.log(playlistData)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight
+      const scrollTop = window.scrollY
+      const clientHeight = window.innerHeight
+
+      if (scrollHeight - scrollTop <= clientHeight && !isFetchingNextPage && hasNextPage) {
+        fetchNextPage()
+        fetchNextPlaylistPage()
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isFetchingNextPage, hasNextPage, fetchNextPage, isFetchingNextPlaylistPage, hasNextPlaylistPage, fetchNextPlaylistPage])
+
+  // 로딩, 에러 처리 추후 수정 필요
+  if (isLoading || isLoadingPlaylist) return <p>Loading...</p>
+  if (error || errorPlaylist) return <p>{(error as Error).message}</p>
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-6">
-      <h1 className="mt-10 p-8">Supabase 연동</h1>
-      <ul>
-        {users?.map((user) => (
-          <li className="text-4xl font-black" key={user.id}>
-            {user.name}
+    <>
+      <ul className="flex min-h-screen flex-col items-center justify-center p-[15px]">
+        {playlistData?.pages.map((page) => page.items.map((item: YouTubePlaylistItem) => <PlayListItem key={item.id.playlistId} item={item} />))}
+      </ul>
+      <ul className="flex min-h-screen flex-col items-center justify-center p-[15px]">
+        {data?.pages.map((page) => page.items.map((item: YouTubeVideoItem) => <VideoItem key={item.id} item={item} />))}
+        {isFetchingNextPage && (
+          <li className="w-full py-4 text-center">
+            <p>더 많은 동영상을 불러오는 중...</p>
           </li>
-        ))}
+        )}
       </ul>
-      <h1 className="mt-10 p-8">Youtube API 연동</h1>
-      <ul>
-        <li>ID: {video.items[0].id}</li>
-        <li>TITLE: {video.items[0].snippet.localized.title}</li>
-        <li>DESCRIPTION: {video.items[0].snippet.localized.description}</li>
-        <li>IMG URL: {video.items[0].snippet.thumbnails.standard.url}</li>
-      </ul>
-    </div>
+    </>
   )
 }
 
