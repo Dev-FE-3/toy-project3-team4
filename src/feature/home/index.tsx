128b735b8799b284a33c7710b8703c2e580234da
mirinae700 <mirinae219@gmail.com>
2025-04-09 13:12:45 +0900

    Refactor: li 태그 중복되어 삭제

--- src/feature/home/index.tsx
+++ src/feature/home/index.tsx
@@ -20,11 +20,7 @@ const Home: React.FC = () => {
   return (
     <ul className="flex min-h-screen flex-col items-center p-[15px]">
       {data?.pages.map((page) => page.items.map((item: YouTubeVideoItem) => <VideoItem key={item.id} item={item} />))}
-      {isFetchingNextPage && (
-        <li className="mb-5 w-full">
-          <VideoItemSkeleton />
-        </li>
-      )}
+      {isFetchingNextPage && <VideoItemSkeleton />}
     </ul>
   )
 }
