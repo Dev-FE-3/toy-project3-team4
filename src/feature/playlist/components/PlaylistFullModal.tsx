a1cb5b320e0265fa97cf534cb50609787b207bb6
jungu0416 <jungu0416@naver.com>
2025-04-12 11:47:47 +0900

    Refactor: IPlaylistFullModalProps 수정

--- src/feature/playlist/components/PlaylistFullModal.tsx
+++ src/feature/playlist/components/PlaylistFullModal.tsx
@@ -14,9 +14,6 @@ export interface IPlaylistFullModalProps {
         channelTitle: string
       }
     }[]
-    pageInfo: {
-      resultsPerPage: number
-    }
   }
   myself: boolean
   playlist: {
