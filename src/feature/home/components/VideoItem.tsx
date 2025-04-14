f6cde2e59da8b8396a64d734d77e2c42c06b1642
mirinae700 <mirinae219@gmail.com>
2025-04-14 22:32:53 +0900

    Feat: 홈에 비디오 북마크 기능 적용

--- src/feature/home/components/VideoItem.tsx
+++ src/feature/home/components/VideoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { memo, useCallback } from 'react'
 import { Bookmark } from 'lucide-react'
 import { VideoItemProps } from '../type/IVideoTypes'
 import useChannelInfo from '../api/useChannelInfo'
@@ -6,33 +6,43 @@ import VideoThumbnail from '@/shared/components/video/VideoThumnail'
 import ChannelAvatar from '@/shared/components/video/ChannelAvatar'
 import VideoInfo from '@/shared/components/video/VideoInfo'
 import VideoItemSkeleton from './VideoItemSkeleton'
+import MoreOptions from '@/shared/components/more-options/MoreOptions'
 
-const VideoItem = React.memo(({ item }: VideoItemProps) => {
+const VideoItem = memo(({ item, onVideoSelect }: VideoItemProps) => {
   const { data: channelData, isLoading: isChannelLoading } = useChannelInfo(item.snippet.channelId)
   const channelThumbnail = channelData?.items[0]?.snippet?.thumbnails?.default?.url
 
-  const handleBookmarkClick = React.useCallback(() => {
-    console.log('bookmark clicked')
-  }, [])
+  const handleBookmarkClick = useCallback(() => {
+    onVideoSelect(typeof item.id === 'string' ? item.id : item.id.videoId)
+  }, [item.id, onVideoSelect])
+
+  const menuItems = [
+    {
+      icon: <Bookmark size={20} className="cursor-pointer stroke-gray-dark" strokeWidth={1.5} />,
+      label: '플레이리스트에 추가',
+      onClick: handleBookmarkClick,
+    },
+  ]
 
   if (isChannelLoading) return <VideoItemSkeleton />
 
   return (
-    <li className="mb-5 w-full">
-      <VideoThumbnail videoId={typeof item.id === 'string' ? item.id : item.id.videoId} thumbnailUrl={item.snippet.thumbnails.high.url} />
+    <>
+      <li className="mb-5 w-full">
+        <VideoThumbnail videoId={typeof item.id === 'string' ? item.id : item.id.videoId} thumbnailUrl={item.snippet.thumbnails.high.url} />
 
-      <div className="flex items-start gap-3">
-        <ChannelAvatar channelId={item.snippet.channelId} channelThumbnail={channelThumbnail} />
-        <VideoInfo
-          videoId={typeof item.id === 'string' ? item.id : item.id.videoId}
-          title={item.snippet.title}
-          viewCount={Number(item.statistics.viewCount)}
-          publishedAt={item.snippet.publishedAt}
-        />
-        {/* 버튼 바꾸기 */}
-        <Bookmark size={20} className="cursor-pointer stroke-gray-dark" strokeWidth={1.5} onClick={handleBookmarkClick} />
-      </div>
-    </li>
+        <div className="flex items-start gap-3">
+          <ChannelAvatar channelId={item.snippet.channelId} channelThumbnail={channelThumbnail} />
+          <VideoInfo
+            videoId={typeof item.id === 'string' ? item.id : item.id.videoId}
+            title={item.snippet.title}
+            viewCount={Number(item.statistics.viewCount)}
+            publishedAt={item.snippet.publishedAt}
+          />
+          <MoreOptions items={menuItems} />
+        </div>
+      </li>
+    </>
   )
 })
 
--- src/feature/home/index.tsx
+++ src/feature/home/index.tsx
@@ -4,8 +4,11 @@ import { useInfiniteScroll } from '@/shared/hooks/useInfiniteScroll'
 import VideoItem from './components/VideoItem'
 import VideoItemSkeleton from './components/VideoItemSkeleton'
 import { YouTubeVideoItem } from '@/shared/type/IYouTubeTypes'
+import ModalManager from '@/shared/components/playlist-modal/ModalManager'
+import { useVideoState } from './hooks/useVideoState'
 
 const Home: React.FC = () => {
+  const { videoId, handleVideoSelect } = useVideoState()
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, error } = useVideoList()
 
   useInfiniteScroll({
@@ -18,12 +21,17 @@ const Home: React.FC = () => {
   if (error) return <p>{(error as Error).message}</p>
 
   return (
-    <ul className="flex min-h-screen flex-col items-center p-[15px]">
-      {data?.pages.map((page) =>
-        page.items.map((item: YouTubeVideoItem) => <VideoItem key={typeof item.id === 'string' ? item.id : item.id.videoId} item={item} />),
-      )}
-      {isFetchingNextPage && <VideoItemSkeleton />}
-    </ul>
+    <>
+      <ul className="flex min-h-screen flex-col items-center p-[15px]">
+        {data?.pages.map((page) =>
+          page.items.map((item: YouTubeVideoItem) => (
+            <VideoItem key={typeof item.id === 'string' ? item.id : item.id.videoId} item={item} onVideoSelect={handleVideoSelect} />
+          )),
+        )}
+        {isFetchingNextPage && <VideoItemSkeleton />}
+      </ul>
+      <ModalManager videoId={videoId} />
+    </>
   )
 }
 
