063ff6e936d334493db0784898229cab9b29c9ed
redbuttonking <103170787+redbuttonking@users.noreply.github.com>
2025-04-15 17:22:10 +0900

    Fix: Link (myself=true) 오타 수정

--- src/feature/channel/components/PlayLisyItem.tsx
+++ src/feature/channel/components/PlayLisyItem.tsx
@@ -23,7 +23,7 @@ const PlayListItem: React.FC<IPlayListIemProps> = ({ id, name, access, created_a
   return (
     <li key={id} className="flex h-[120px] justify-between gap-[15px]">
       <section className="relative w-[180px]">
-        <Link to={`/watch?video=${videoId}&playlist=${playListId}&myself=ture`}>
+        <Link to={`/watch?video=${videoId}&playlist=${playListId}&myself=true`}>
           <div className="ml-2 mr-2 h-[50px] rounded-[5px] bg-[#E0E0E2]"></div>
           <img className="absolute top-[8px] w-[180px] rounded-[5px]" src={videoThumbnail} />
           <div className="absolute bottom-[20px] right-[9px] flex w-[48px] justify-center gap-[4px] rounded-[20px] bg-gray-900/40 px-[8px] py-[3px] text-white">
@@ -34,7 +34,7 @@ const PlayListItem: React.FC<IPlayListIemProps> = ({ id, name, access, created_a
       </section>
 
       <section className="mt-[5px] flex w-[205px] justify-between gap-[10px]">
-        <Link to={`/watch?video=${videoId}&playlist=${playListId}&myself=ture`}>
+        <Link to={`/watch?video=${videoId}&playlist=${playListId}&myself=true`}>
           <h3 className="flex w-[180px] flex-col gap-[8px]">
             <h3 className="text-[14px] font-semibold">{name}</h3>
             <h3 className="text-[12px] text-gray-medium-dark">{access === 'true' ? '공개' : '비공개'}</h3>
