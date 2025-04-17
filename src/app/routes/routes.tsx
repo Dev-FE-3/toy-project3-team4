06b979066f0e69dafef19b122c39194f98f37fbb
mirinae700 <mirinae219@gmail.com>
2025-04-17 16:02:26 +0900

    Remove: 디자인 가이드 페이지 삭제

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from '../layout/Layout'
 import Home from '@/feature/home'
-import Design from '@/feature/design/Design'
 import Video from '@/feature/video'
 import AppSettings from '@/feature/app-settings'
 import Login from '@/feature/login'
@@ -20,7 +19,6 @@ function AppRoutes() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
-          <Route path="/design-guide" element={<Design />} />
           <Route path="/watch" element={<Video />} />
           <Route path="/settings" element={<AppSettings />} />
           <Route path="/playlist" element={<PlayListView />} />
